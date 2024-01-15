b9a3d4e332a3464a2fde65c8777fb46a756d7e31
Greg Frasco <frascog17@gmail.com>
2024-01-15 10:04:02 -0800

    feat: update cookies

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -9,11 +9,13 @@ export const Video: FC<VideoProps> = ({ videoId }) => {
   return (
     <Box sx={{ position: 'relative', overflow: 'hidden', width: '100%', paddingTop: '49%' }}>
       <iframe
+        allowFullScreen
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, width: '100%', height: '100%', border: 'none' }}
         id="ytplayer"
         width="640"
         height="360"
-        src={`https://www.youtube.com/embed/${videoId}?loop=1&autoplay=1&controls=0&mute=1&version=3&playlist=${videoId}`}
+        src={`https://www.youtube-nocookie.com/embed/${videoId}?si=pywu5qpo4E1r9qvL&loop=1&autoplay=1&controls=0&mute=1&version=3&playlist=${videoId}`}
       ></iframe>
     </Box>
   );
