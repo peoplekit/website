21adf9cf48ede1917b43f946cb5047b26b5489ab
Greg Frasco <frascog17@gmail.com>
2025-10-01 18:10:24 -0400

    feat: install posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import {PostHogProvider} from "posthog-js/react";
+
+const posthogOptions = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-05-24',
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={posthogOptions}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
